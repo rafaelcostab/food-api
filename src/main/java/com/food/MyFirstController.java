7abd2f287f1652e53f32d9cccede004f1005ee7e
Rafael Costa <rafael.costab@hotmail.com>
2020-02-14 00:07:35 -0300

    creating my first controller

---
+++ src/main/java/com/food/MyFirstController.java
@@ -0,0 +1,15 @@
+package com.food;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyFirstController {
+	
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return "Hello World!";
+	}
+}
