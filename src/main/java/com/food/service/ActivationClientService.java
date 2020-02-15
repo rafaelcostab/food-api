e6f42ffa634607fa4e489d39b219040f160a3b4e
Rafael Costa <rafael.costab@hotmail.com>
2020-02-15 00:01:58 -0300

    2.14 acknowledging the injection points and the annotation @Autowired

--- src/main/java/com/food/service/ActivationClientService.java
+++ src/main/java/com/food/service/ActivationClientService.java
@@ -1,5 +1,6 @@
 package com.food.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.food.model.Client;
@@ -8,16 +9,31 @@ import com.food.notification.Notificator;
 @Component
 public class ActivationClientService {
 	
+	// exemple autowired in attribute
+	@Autowired
 	private Notificator notificator;
 	
+	/* example autowired in contructor method
+	@Autowired
 	public ActivationClientService(Notificator notificator) {
 		this.notificator = notificator;
 	}
 
+	public ActivationClientService(String any) {
+		
+	}
+	*/
+	
 	public void activite(Client client) {
 		client.activate();
 		
 		notificator.notify(client, "Your register in system is activated!");
 	}
-	
+
+	/* example autowired in set method
+	@Autowired
+	public void setNotificator(Notificator notificator) {
+		this.notificator = notificator;
+	}
+	*/
 }
