55bf039ffca6fe2942811e2a270a58946b49f498
Rafael Costa <rafael.costab@hotmail.com>
2020-02-28 17:17:55 -0300

    removing exemple spring profiles

--- src/main/java/com/food/notification/NotificatorEmail.java
+++ src/main/java/com/food/notification/NotificatorEmail.java
@@ -1,18 +1,12 @@
 package com.food.notification;
 
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import com.food.model.Client;
 
-@Profile("production")
 @NotificatorType(UrgentNivel.NORMAL)
 @Component
 public class NotificatorEmail implements Notificator {
-	
-	public NotificatorEmail() {
-		System.out.println("Starting: NotificatorEmail");
-	}
 	
 	@Override
 	public void notify(Client client, String message) {	
