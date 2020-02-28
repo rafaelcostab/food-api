02291fcbff1908b6423b74cb0f52c9ae380827fd
Rafael Costa <rafael.costab@hotmail.com>
2020-02-28 17:22:32 -0300

    2.21 creating method callback life cycle of beans
    first implementation example: using anotations

--- src/main/java/com/food/service/ActivationClientService.java
+++ src/main/java/com/food/service/ActivationClientService.java
@@ -1,5 +1,8 @@
 package com.food.service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +18,16 @@ public class ActivationClientService {
 	@Autowired
 	private Notificator notificator;
 		
+	@PostConstruct
+	public void init() {
+		System.out.println("Init");
+	}
+	
+	@PreDestroy
+	public void destroy() {
+		System.out.println("Destroy");
+	}
+	
 	public void activite(Client client) {
 		client.activate();
 		
