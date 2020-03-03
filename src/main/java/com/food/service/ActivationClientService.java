b6d357cc0839b43cd6c1b5711df747b158d42174
Rafael Costa <rafael.costab@hotmail.com>
2020-03-03 00:53:57 -0300

    removing example methods callback

--- src/main/java/com/food/service/ActivationClientService.java
+++ src/main/java/com/food/service/ActivationClientService.java
@@ -1,41 +1,24 @@
 package com.food.service;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.food.model.Client;
 import com.food.notification.Notificator;
 import com.food.notification.NotificatorType;
 import com.food.notification.UrgentNivel;
 
-//@Component
-public class ActivationClientService implements InitializingBean, DisposableBean {
+@Component
+public class ActivationClientService {
 
 	@NotificatorType(UrgentNivel.URGENT)
 	@Autowired
 	private Notificator notificator;
-		
-	//@PostConstruct
-	public void init() {
-		System.out.println("Init");
-	}
-	
-	//@PreDestroy
-	public void destroy() {
-		System.out.println("Destroy");
-	}
-	
+
 	public void activite(Client client) {
 		client.activate();
 		
 		notificator.notify(client, "Your register in system this activated!");
-	}
-
-	@Override
-	public void afterPropertiesSet() throws Exception {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
