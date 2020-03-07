12e43525a474322b181aa334d07276a7175c17c0
Rafael Costa <rafael.costab@hotmail.com>
2020-03-07 18:32:34 -0300

    2.25 Creating and accessing costumers properties with anotation Value

--- src/main/java/com/food/notification/NotificatorEmail.java
+++ src/main/java/com/food/notification/NotificatorEmail.java
@@ -1,5 +1,6 @@
 package com.food.notification;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.food.model.Client;
@@ -8,8 +9,17 @@ import com.food.model.Client;
 @Component
 public class NotificatorEmail implements Notificator {
 	
+	@Value("${notificator.email.server-host}")
+	private String host;
+	
+	@Value("$notificator.email.server-port")
+	private Integer port;
+	
 	@Override
 	public void notify(Client client, String message) {	
+		System.out.println("Host: " + host);
+		System.out.println("Port: " + port);
+		
 		System.out.printf("notifying %s through the email %s: %s\n",
 				client.getName(), client.getEmail(), message);
 	}	
