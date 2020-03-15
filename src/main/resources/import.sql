ac2019e5b52934736ed2ef5ae649c3602af3d736
Rafael Costa <rafael.costab@hotmail.com>
2020-03-15 01:06:09 -0300

    3.7 importing  test data with import sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into kitchens (name) values ('Thai');
+insert into kitchens (name) values ('Indian');
+insert into kitchens (name) values ('Brasilian');
+insert into kitchens (name) values ('Chinese');
+insert into kitchens (name) values ('Japanese');
