7661424ca538064a1174706fee56a2f5cf03be75
Rafael Costa <rafael.costab@hotmail.com>
2020-03-18 00:24:18 -0300

    3.16 adding command insert the restaurants on import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
 insert into kitchen (name) values ('Thai');
 insert into kitchen (name) values ('Indian');
+insert into restaurant (name, tax_freight) values ('Jabaquara Corner', 6.0);
+insert into restaurant (name, tax_freight) values ('Bony', 66.0);
