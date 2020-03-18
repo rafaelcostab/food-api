59861b42250b6de81a6d0593726a8d41b65529a7
Rafael Costa <rafael.costab@hotmail.com>
2020-03-18 01:02:02 -0300

    3.16 testing implementation restaurant: find by id

---
+++ src/main/java/com/food/jpa/FindRestaurantMain.java
@@ -0,0 +1,26 @@
+package com.food.jpa;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import com.food.FoodApiApplication;
+import com.food.domain.model.Restaurant;
+import com.food.domain.repository.RestaurantRepository;
+
+public class FindRestaurantMain {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new SpringApplicationBuilder(FoodApiApplication.class)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		RestaurantRepository restaurants = applicationContext.getBean(RestaurantRepository.class);
+		
+		Restaurant restaurant = restaurants.findById(1L);
+
+		System.out.println(restaurant.getName());
+		
+	}
+
+}
