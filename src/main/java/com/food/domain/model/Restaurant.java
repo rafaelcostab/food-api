f89d3a0c0577de118cfc20dbfd166084f045a684
Rafael Costa <rafael.costab@hotmail.com>
2020-03-21 00:25:01 -0300

    3.18 The anotation Join Column

--- src/main/java/com/food/domain/model/Restaurant.java
+++ src/main/java/com/food/domain/model/Restaurant.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import lombok.Data;
@@ -28,6 +29,7 @@ public class Restaurant {
 	private BigDecimal taxFreight;
 	
 	@ManyToOne
+	@JoinColumn(name = "kitchen_id")
 	private Kitchen kitchen;
 	
 }
