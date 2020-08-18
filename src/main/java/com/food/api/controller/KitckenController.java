95d7ab8f79c57013238d31ec6ea4b070baca0d8e
Rafael Costa <rafael.costab@hotmail.com>
2020-08-17 23:55:02 -0300

    4.13 implementing content negotiation to return JSON or XML

--- src/main/java/com/food/api/controller/KitckenController.java
+++ src/main/java/com/food/api/controller/KitckenController.java
@@ -3,6 +3,7 @@ package com.food.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +12,21 @@ import com.food.domain.model.Kitchen;
 import com.food.domain.repository.KitchenRepository;
 
 @RestController
-@RequestMapping("/kitchens")
+@RequestMapping(value = "/kitchens") //, produces = MediaType.APPLICATION_JSON_VALUE)
 public class KitckenController {
 	
 	@Autowired
 	private KitchenRepository kitchenRepository;
 	
-	@GetMapping
-	public List<Kitchen> list(){
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Kitchen> list1(){
+		System.out.println("List 1");
+		return kitchenRepository.findAll();
+	}
+	
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	public List<Kitchen> list2(){
+		System.out.println("List 2");
 		return kitchenRepository.findAll();
 	}
 	
