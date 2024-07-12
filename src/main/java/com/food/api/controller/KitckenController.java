8045f7df34614a9e2935418c5ddafc28c645cda4
Rafael Costa <rafael.costab@hotmail.com>
2024-07-12 00:40:55 -0300

    4.19 setting the HTTP response with @ResponseStatus

--- src/main/java/com/food/api/controller/KitckenController.java
+++ src/main/java/com/food/api/controller/KitckenController.java
@@ -3,11 +3,13 @@ package com.food.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.food.api.model.KitchensXmlWrapper;
@@ -31,14 +33,10 @@ public class KitckenController {
 		return new KitchensXmlWrapper(kitchenRepository.findAll());
 	}
 	
+	@ResponseStatus(HttpStatus.CREATED)
 	@GetMapping("/{kitchenId}")
 	public Kitchen find(@PathVariable Long kitchenId){
 		return kitchenRepository.findById(kitchenId);
-	}
-	
-	@GetMapping("/by-name")
-	public List<Kitchen> findByName(@RequestParam("name") String name){
-		return kitchenRepository.findByName(name);
 	}
 
 }
