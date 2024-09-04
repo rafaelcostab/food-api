a3b457edb824e9515d2a45dca89b956160fcd710
Rafael Costa <rafael.costab@hotmail.com>
2024-09-03 21:54:12 -0300

    4.26 modeling and implementing the exclusion of resources with delete

--- src/main/java/com/food/api/controller/KitckenController.java
+++ src/main/java/com/food/api/controller/KitckenController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.info.ProjectInfoProperties.Build;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -72,4 +74,24 @@ public class KitckenController {
 		
 	}
 
+	@DeleteMapping("/{kitchenId}")
+	public ResponseEntity<Kitchen> remove(@PathVariable Long kitchenId){
+		
+		try {
+			Kitchen kitchen = kitchenRepository.findById(kitchenId);
+
+			if (kitchen != null) {
+				
+				kitchenRepository.remove(kitchen);
+				
+				return ResponseEntity.noContent().build();
+				
+			}
+			
+			return ResponseEntity.notFound().build();
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+		}
+	}
+	
 }
