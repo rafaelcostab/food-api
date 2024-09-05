1e0dfd51d1344a2bb8e55358525e3c3a3eb38215
Rafael Costa <rafael.costab@hotmail.com>
2024-09-04 23:57:59 -0300

    4.29 removing xml support

--- src/main/java/com/food/api/controller/KitckenController.java
+++ src/main/java/com/food/api/controller/KitckenController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.food.api.model.KitchensXmlWrapper;
 import com.food.domain.exception.EntityInUseException;
 import com.food.domain.exception.EntityNotFoundException;
 import com.food.domain.model.Kitchen;
@@ -37,11 +35,6 @@ public class KitckenController {
 	@GetMapping
 	public List<Kitchen> list(){
 		return kitchenRepository.findAll();
-	}
-	
-	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-	public KitchensXmlWrapper findXml() {
-		return new KitchensXmlWrapper(kitchenRepository.findAll());
 	}
 	
 	@GetMapping("/{kitchenId}")
